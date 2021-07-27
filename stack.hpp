b2722bdda000098bbea2b52f11e768de089a0a5b
gfratini <giuliof32@gmail.com>
2021-07-27 15:19:18 +0200

    implement stack, tests still to be made

--- stack.hpp
+++ stack.hpp
@@ -18,46 +18,74 @@ namespace ft {
 		container_type c;
 
 	public:
-		stack();
-		~stack();
+		stack() {};
+		~stack() {};
 
-		stack(const stack& q);
-		stack(stack& q);
+		stack(const stack& q) {
+			*this = q;
+		}
+		stack(stack& q) {
+			*this = q;
+		}
+		stack& operator=(stack const& q) {
+			c = q.c;
+		}
 
-		stack& operator=(const stack& q);
-		stack& operator=(stack& q);
+		explicit stack(const container_type& c) {
+			this->c = c;
+		}
 
-		explicit stack(const container_type& c);
-		explicit stack(container_type& c);
+		bool empty() const {
+			return c.empty();
+		}
+		size_type size() const {
+			return c.size();
+		}
+		reference top() {
+			return c.back();
+		}
+		const_reference top() const {
+			return c.back();
+		}
 
-		bool empty() const;
-		size_type size() const;
-		reference top();
-		const_reference top() const;
-
-		void push(const value_type& x);
-		void push(value_type& x);
-		void pop();
-
-		void swap(stack& c) noexcept(is_nothrow_swappable_v<Container>)
+		void push(const value_type& x) {
+			c.push_back(x);
+		}
+		void push(value_type& x) {
+			c.push_back(x);
+		}
+		void pop() {
+			c.pop_back();
+		}
+		friend
+		bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs) { return lhs.c == rhs.c; }
+		friend
+		bool operator>  (const stack<T,Container>& lhs, const stack<T,Container>& rhs) { return lhs.c > rhs.c; }
 	};
 
 	template <class T, class Container>
-	bool operator==(const stack<T, Container>& x, const stack<T, Container>& y);
+	bool operator!= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+		return !(lhs == rhs);
+	}
+
 	template <class T, class Container>
-	bool operator< (const stack<T, Container>& x, const stack<T, Container>& y);
+	bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+		return !(lhs > rhs || lhs == rhs);
+	}
+
 	template <class T, class Container>
-	bool operator!=(const stack<T, Container>& x, const stack<T, Container>& y);
+	bool operator<= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+		return !(lhs > rhs);
+	}
+
 	template <class T, class Container>
-	bool operator> (const stack<T, Container>& x, const stack<T, Container>& y);
-	template <class T, class Container>
-	bool operator>=(const stack<T, Container>& x, const stack<T, Container>& y);
-	template <class T, class Container>
-	bool operator<=(const stack<T, Container>& x, const stack<T, Container>& y);
+	bool operator>= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {
+		return !(lhs < rhs);
+	}
 
 	template <class T, class Container>
 	void swap(stack<T, Container>& x, stack<T, Container>& y) {
 		x.swap(y);
 	}
 
-}
+} //namespace ft
