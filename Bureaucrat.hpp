7e1efef9cdf5d79b6742e8581c8a0ccf3a5ba637
franzudev <fede.franzu@gmail.com>
2021-07-28 18:09:32 +0200

    add overload to class Bureaucrat

--- Bureaucrat.hpp
+++ Bureaucrat.hpp
@@ -21,7 +21,20 @@ public:
 		return grade;
 	}
 
+	friend
+	bool operator== (const Bureaucrat lhs, const Bureaucrat rhs) { return lhs.getGrade() == rhs.getGrade(); }
+	friend
+	bool operator>  (const Bureaucrat lhs, const Bureaucrat rhs) { return lhs.getGrade() > rhs.getGrade(); }
+	friend
+	bool operator!= (const Bureaucrat& lhs, const Bureaucrat& rhs) {return !(lhs == rhs);}
+	friend
+	bool operator<  (const Bureaucrat& lhs, const Bureaucrat& rhs) {return !(lhs > rhs || lhs == rhs);}
+	friend
+	bool operator<= (const Bureaucrat& lhs, const Bureaucrat& rhs) {return !(lhs > rhs);}
+	friend
+	bool operator>= (const Bureaucrat& lhs, const Bureaucrat& rhs) {return !(lhs < rhs);}
 };
+
 
 std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat){
 	os <<  bureaucrat.getName() << ", " << bureaucrat.getGrade() << std::endl;
