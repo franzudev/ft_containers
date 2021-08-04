5742d56213db329a0412888dbbe3ac021755094d
gfratini <giuliof32@gmail.com>
2021-08-04 12:04:19 +0200

    add iterators (still to be implemented)

--- map.hpp
+++ map.hpp
@@ -9,26 +9,95 @@
 
 namespace ft {
 
+	template<class Pointer>
+	struct map_iterator {
+		typedef Pointer                                                     iterator_type;
+		typedef typename iterator_traits<iterator_type>::iterator_category	iterator_category;
+		typedef typename iterator_traits<iterator_type>::value_type			value_type;
+		typedef typename iterator_traits<iterator_type>::difference_type	difference_type;
+		typedef typename iterator_traits<iterator_type>::pointer			pointer;
+		typedef typename iterator_traits<iterator_type>::reference         	reference;
+
+		explicit map_iterator(): m_ptr(nullptr){};
+
+		explicit map_iterator(iterator_type ptr): m_ptr(ptr) {}
+
+		template<class Iter>
+		map_iterator(map_iterator<Iter> const & other) {
+			*this = other;
+		}
+
+		reference operator*() const { return *m_ptr; }
+
+		pointer operator->() const { return m_ptr; }
+
+		// Prefix increment
+		map_iterator &operator++() {
+			if (m_ptr->right)
+				m_ptr = m_ptr->right;
+			else if (m_ptr->isLeft()) {
+				m_ptr = m_ptr->parent;
+			} else {
+				while (m_ptr->parent && !m_ptr->isLeft())
+					m_ptr = m_ptr->parent;
+				m_ptr = m_ptr->parent;
+			}
+			return *this;
+		}
+
+		// Postfix increment
+		map_iterator operator++(int) { map_iterator tmp = *this; ++*this; return tmp; }
+
+		// Prefix decrement
+		map_iterator &operator--() {
+			if (m_ptr->left)
+				m_ptr = m_ptr->left;
+			else if (m_ptr->isRight()) {
+				m_ptr = m_ptr->parent;
+			} else {
+				while (m_ptr->parent && !m_ptr->isRight())
+					m_ptr = m_ptr->parent;
+				m_ptr = m_ptr->parent;
+			}
+			return *this;
+		}
+
+		// Postfix decrement
+		map_iterator operator--(int) { map_iterator tmp = *this; --*this; return tmp; }
+
+		pointer	base() const {
+			return m_ptr;
+		}
+
+		template <class Iter>
+		map_iterator&	operator=(map_iterator<Iter> const & other) {
+			m_ptr = other.base();
+			return *this;
+		}
+	private:
+		iterator_type m_ptr;
+	};
+
 	template < class Key, class T, class Compare = less<Key>, class Alloc = std::allocator<ft::pair<const Key,T> > >
 	class map {
 	public:
 		// --- Definitions
-		typedef Key                                     		key_type;
-		typedef T                                       		mapped_type;
-		typedef pair<const key_type, mapped_type>       		value_type;
-		typedef Compare                                 		key_compare;
-		typedef Allocator                               		allocator_type;
-		typedef typename allocator_type::reference      		reference;
+		typedef Key												key_type;
+		typedef T												mapped_type;
+		typedef pair<const key_type, mapped_type>				value_type;
+		typedef Compare											key_compare;
+		typedef Alloc											allocator_type;
+		typedef typename allocator_type::reference				reference;
 		typedef typename allocator_type::const_reference		const_reference;
-		typedef typename allocator_type::pointer        		pointer;
-		typedef typename allocator_type::const_pointer  		const_pointer;
-		typedef typename allocator_type::size_type      		size_type;
+		typedef typename allocator_type::pointer				pointer;
+		typedef typename allocator_type::const_pointer			const_pointer;
+		typedef typename allocator_type::size_type				size_type;
 		typedef typename allocator_type::difference_type		difference_type;
 
-		typedef ft::iterator<ft::bidirectional_iterator_tag, T>	iterator;
-		typedef ft::iterator<ft::bidirectional_iterator_tag, T> const_iterator;
-		typedef std::reverse_iterator<iterator>         		reverse_iterator;
-		typedef std::reverse_iterator<const_iterator>   		const_reverse_iterator;
+		typedef ft::map_iterator<pointer>						iterator;
+		typedef ft::map_iterator<const_pointer>					const_iterator;
+		typedef std::reverse_iterator<iterator>					reverse_iterator;
+		typedef std::reverse_iterator<const_iterator>			const_reverse_iterator;
 		// definitions
 
 		class value_compare: public binary_function<value_type, value_type, bool>
@@ -90,7 +159,7 @@ namespace ft {
 			iterator(tree.insert(value));
 		}
 		template< class InputIt >
-				void insert( InputIt first, InputIt last );
+		void insert( InputIt first, InputIt last );
 		void erase( iterator pos ) {}
 		void erase( iterator first, iterator last ) {}
 		size_type erase( const key_type& key ) {}
